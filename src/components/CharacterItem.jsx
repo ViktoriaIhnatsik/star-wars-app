0a394395fca760fa27893688ffa2a03e99043a44
Viktoria Ihnatsik <ignatik_viktoria@hotmail.com>
2021-10-22 21:07:27 +0200

    Added detail info for each character

--- src/components/CharacterItem.jsx
+++ src/components/CharacterItem.jsx
@@ -1,11 +1,52 @@
-import React from 'react'
+import React, { useState} from 'react'
 
 export default function CharacterItem({character}) {
+
+  const [detailInfo, setDetailInfo] = useState(false);
+
+  const handleOnClick = () => {
+    detailInfo ? setDetailInfo(false) : setDetailInfo(true);
+  };
+
  return (
    <div className="col-md-4 shadow p-3">
      {character.name}
      <div>
-       <button className="btn btn-secondary btn-block">Detail info</button>
+       <button className="btn btn-secondary btn-block" onClick={handleOnClick}>
+         Detail info
+       </button>
+       {detailInfo && (
+         <table>
+           <tr>
+             <th>Height</th>
+             <td>{character.height}</td>
+           </tr>
+           <tr>
+             <th>Mass</th>
+             <td>{character.mass}</td>
+           </tr>
+           <tr>
+             <th>Hair Color</th>
+             <td>{character.hair_color}</td>
+           </tr>
+           <tr>
+             <th>Skin Color</th>
+             <td>{character.skin_color}</td>
+           </tr>
+           <tr>
+             <th>Eye Color</th>
+             <td>{character.eye_color}</td>
+           </tr>
+           <tr>
+             <th>Birth year</th>
+             <td>{character.birth_year}</td>
+           </tr>
+           <tr>
+             <th>Gender</th>
+             <td>{character.gender}</td>
+           </tr>
+         </table>
+       )}
      </div>
    </div>
  );
